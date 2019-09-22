f10720d8ec044c65f35a72131cd1208bed96965b
i-am-phi <phi@photon-platform.net>
2019-09-21 23:10:08 -0700

    added date time to file name

--- sh/timecut.sh
+++ sh/timecut.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+D=$(date +"%Y%m%d-%T")
 
 node node_modules/timecut/cli.js "$1/$1.svg.html" \
   --viewport=1920,1280 \
-  --fps=8 \
+  --fps=16 \
   --duration=344 \
   --start-delay=0 \
   --frame-cache timecut \
   --pix-fmt=yuv420p \
-  --output="$1/video/$1.mp4"
+  --output="$1/video/$1.$D.mp4"
