8e93bfe02f9bc73caa3de8cb5526099ec3a2d799
phiarchitect <phi@phiarchitect.com>
2020-12-22 13:08:07 -0800

    change timecut URL

--- sh/timecut.sh
+++ sh/timecut.sh
@@ -3,7 +3,7 @@ D=$(date +"%Y%m%d-%T")
 FPS=24
 DURATION=10
 
-node node_modules/timecut/cli.js "$1/index.html" \
+node node_modules/timecut/cli.js "http://constructions.local/$1" \
   --viewport=1920,1080 \
   --fps=$FPS\
   --duration=$DURATION \
